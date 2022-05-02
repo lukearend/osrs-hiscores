d9af83d9e8cbd622b89fa34469b846b10308992c
Luke Arend <larend@mit.edu>
2022-05-02 00:48:40 -0500

    run on 0.0.0.0 if ON_HEROKU set in env
    Former-commit-id: 075d1322b81f60ced7b075833c28af77c52f0151

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 buildapp(app, mongo_url, appdata_coll, appdata_file, auth)
 
 server = app.server
-app.run_server(debug=debug, port=os.getenv('PORT', 8050))
+host = "0.0.0.0" if os.getenv('ON_HEROKU', False) else 'localhost'
+app.run_server(debug=debug, host=host, port=os.getenv('PORT', 8050))
