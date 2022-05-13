83ef4a0ccb0e7298fd98594ed9bae602f49f35cc
Luke Arend <larend@mit.edu>
2022-05-13 18:09:08 -0500

    add colors.py with initial color scheme
    Former-commit-id: e3d12daef49c991bf8a2601fdc56dd6a070ca07f

---
+++ src/app/colors.py
@@ -0,0 +1,39 @@
+import plotly.express as px
+
+white = '#ffffff'
+off_white = '#eeeeee'
+off_black = '#111111'
+bg_gray = '#222222'
+panel_gray = '#303030'
+highlight_gray = '#555555'
+success_green = '#198754'
+error_red = '#dc3545'
+
+BG_COLOR = bg_gray
+TEXT_COLOR = off_white
+LINK_COLOR = success_green
+
+INPUT_TEXT = TEXT_COLOR
+INPUT_BG = off_black
+INPUT_BORDER = highlight_gray
+INPUT_PROMPT = highlight_gray
+INPUT_SUCCESS = success_green
+INPUT_FAIL = error_red
+
+DROPDOWN_TEXT = TEXT_COLOR
+DROPDOWN_BG = off_black
+DROPDOWN_BORDER = highlight_gray
+DROPDOWN_ITEM = bg_gray
+DROPDOWN_ITEM_HOVERED = panel_gray
+
+SCATTERPLOT_BG = bg_gray
+SCATTERPLOT_POINTS = white
+HALO_COLORS = px.colors.qualitative.Plotly
+
+TABLE_BG = panel_gray
+TABLE_CELL = off_black
+TABLE_TEXT = TEXT_COLOR
+
+BOXPLOT_BG = off_black
+BOXPLOT_PAPER = BG_COLOR
+BOXPLOT_GRIDLINES = BG_COLOR
