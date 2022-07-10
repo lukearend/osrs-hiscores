748e084e75da0aa39c01188be75bab7cfb0b3052
Luke Arend <larend@mit.edu>
2022-07-09 23:34:37 -0500

    change runtime
    Former-commit-id: 39e4ba0172a8fbe6164fb3ba24ba4e56c035af1b

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='osrs-hiscores',
     packages=find_packages(),
     version='1.0',
-    python_requires='3.9',
+    python_requires='>=3.9',
     description='code for a data study of the OSRS hiscores',
     author='Luke Arend',
     license='MIT',
